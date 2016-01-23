b37e37a24995258eced497bfed6cd34046ec4bc8
Rahul Bobhate <bobhate.rahul@gmail.com>
2016-01-23 17:03:03 -0500

    More changes

--- app/src/main/java/net/stupidiot/foodifai/FoodifaiCaller.java
+++ app/src/main/java/net/stupidiot/foodifai/FoodifaiCaller.java
@@ -6,6 +6,7 @@ import com.clarifai.api.RecognitionResult;
 import com.clarifai.api.Tag;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,16 +16,23 @@ public class FoodifaiCaller
 {
     private static String APP_ID = "yzSmx6Vood4Sej97eqTsCHY5rkU-pK4qS7eQVDWR";
     private static String APP_SECRET = "uR-JBcoi8b5ti-oKeRad4AwgDwm0eQklZWOBOIsK";
+    private static ClarifaiClient clarifai = new ClarifaiClient(APP_ID, APP_SECRET);
 
-    public static String[] getTags(String path)
+    public static List<String> getTags(String path)
     {
-        ClarifaiClient clarifai = new ClarifaiClient(APP_ID, APP_SECRET);
         List<RecognitionResult> results =
                 clarifai.recognize(new RecognitionRequest(new File(path)));
 
+        final List<String> tags = new ArrayList<String>();
+
         for (Tag tag : results.get(0).getTags())
         {
-            System.out.println(tag.getName() + ": " + tag.getProbability());
+            if(tag.getProbability() > 0.5)
+            {
+                tags.add(tag.getName());
+            }
         }
+
+        return tags;
     }
 }
